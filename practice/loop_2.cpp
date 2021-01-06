89149614aefbaf7000fa2f571ef23e20210dda89
jovan bruno <jaredivan.bruno@tup.edu.ph>
2021-01-06 16:15:22 +0800

    Update dup.cpp

--- practice/loop_2.cpp
+++ practice/loop_2.cpp
@@ -1,25 +1,50 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-	int x, y;
-	int sum;
-	cin >> x;
-	cin >> y;
-	sum=x+y;
-	cout << sum << '\n';
+string timeConversion(string s) {
+	if(s[0]=='1' && s[1]=='2' && (s.find("AM")) != string::npos) {
+		s.replace(0, 2, "00");
+	}
+	if(s[0]=='0' && s[1]=='1' && (s.find("PM")) != string::npos) {
+		s.replace(0, 2, "13");
+	}
+	if(s[0]=='0' && s[1]=='2' && (s.find("PM")) != string::npos) {
+		s.replace(0, 2, "14");
+	}
+	if(s[0]=='0' && s[1]=='3' && (s.find("PM")) != string::npos) {
+		s.replace(0, 2, "15");
+	}
+	if(s[0]=='0' && s[1]=='4' && (s.find("PM")) != string::npos) {
+		s.replace(0, 2, "16");
+	}
+	if(s[0]=='0' && s[1]=='5' && (s.find("PM")) != string::npos) {
+		s.replace(0, 2, "17");
+	}
+	if(s[0]=='0' && s[1]=='6' && (s.find("PM")) != string::npos) {
+		s.replace(0, 2, "18");
+	}
+	if(s[0]=='0' && s[1]=='7' && (s.find("PM")) != string::npos) {
+		s.replace(0, 2, "19");
+	}
+	if(s[0]=='0' && s[1]=='8' && (s.find("PM")) != string::npos) {
+		s.replace(0, 2, "20");
+	}
+	if(s[0]=='0' && s[1]=='9' && (s.find("PM")) != string::npos) {
+		s.replace(0, 2, "21");
+	}
+	if(s[0]=='1' && s[1]=='0' && (s.find("PM")) != string::npos) {
+		s.replace(0, 2, "22");
+	}
+	if(s[0]=='1' && s[1]=='1' && (s.find("PM")) != string::npos) {
+		s.replace(0, 2, "23");
+	}
+	s.erase(8, 2);
+	return s;
 }
 
 int main() {
-	ios::sync_with_stdio(NULL);
-	cin.tie(0);
-
-	int t, i=1;
-	cin >> t;
-	while(t--) {
-		cout << "Case #" << i << ": ";
-		solve();
-		i++;
-	}
+	string s;
+	getline(cin, s);
+	
+	cout << timeConversion(s);
 }
-
