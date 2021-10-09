e0a61c7c884247f8ce395d5413fbc9c99995a90d
jared <jaredivan.bruno@tup.edu.ph>
2021-10-10 00:25:37 +0800

    2nd year thingz

--- practice/moneychange.c
+++ practice/moneychange.c
@@ -4,16 +4,22 @@ int main() {
 	int denom[3]={10, 5, 1};
 	int m, ans=0;
 
+	printf("Enter a number: ");
 	scanf("%d", &m);
-
+	
+	printf("%d = ", m);
 	while(m > 0) {
 		for(int i=0; i<3; i++) {
 			if(m - denom[i] >= 0) {
 				m = m - denom[i];
+				printf("%d", denom[i]);
 				ans++;
 				break;
 			}
 		}
+		if(m > 0) {
+			printf(" + ");
+		}
 	}
-	printf("%d\n", ans);
+	printf("\nTotal of %d coin/s\n", ans);
 }
